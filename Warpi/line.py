361592b52996afc224e8a0b107266e10429454ba
Fred <ciphert@proton.me>
2025-01-29 10:15:24 -0700

    Commiting crimes

---
+++ Warpi/line.py
@@ -0,0 +1,17 @@
+# Importing turtle module
+from turtle import *
+color("orange")
+bgcolor("tyedye")
+# Drawing a line fwd 100 steps
+forward(100)
+# Turning left by 120 degrees
+left(120)
+# Drawing a line fwd 100 steps  
+forward(100)
+
+right(240)
+
+forward(100)
+
+# mofuggin triangle bitch
+# also let it be known no ai was used just read through the python turtle documentation
